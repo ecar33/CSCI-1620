1450990f0389c87bf02719f14714d36bb559e531
Evan Carlile <114954921+ecar33@users.noreply.github.com>
2023-04-26 15:10:23 -0500

    Updated lab 11

--- EvanCarlile11_1/area.py
+++ EvanCarlile11_1/area.py
@@ -1,35 +1,47 @@
 import math
 
 
+def check_if_numeric(*args):
+    for arg in args:
+        try:
+            float(arg)
+            return True
+        except ValueError:
+            raise
+
 def rectangle(length, width):
-    if float(length) < 0 or float(width) < 0:
-        raise TypeError
-    else:
-        length = float(length)
-        width = float(width)
-        return length * width
+    if check_if_numeric(length, width):
+        if float(length) < 1 or float(width) < 1:
+            raise TypeError
+        else:
+            length = float(length)
+            width = float(width)
+            return length * width
 
 
 def square(side):
-    if float(side) < 0:
-        raise TypeError
-    else:
-        side = float(side)
-        return side * side
+    if check_if_numeric(side):
+        if float(side) < 1:
+            raise TypeError
+        else:
+            side = float(side)
+            return side * side
 
 
 def circle(radius):
-    if float(radius) < 0:
-        raise TypeError
-    else:
-        radius = float(radius)
-        return (radius * radius) * math.pi
+    if check_if_numeric(radius):
+        if float(radius) < 1:
+            raise TypeError
+        else:
+            radius = float(radius)
+            return (radius * radius) * math.pi
 
 
 def triangle(base, height):
-    if float(base) < 0 or float(height) < 0:
-        raise TypeError
-    else:
-        base = float(base)
-        height = float(height)
-        return 0.5 * base * height
+    if check_if_numeric(base, height):
+        if float(base) < 1 or float(height) < 1:
+            raise TypeError
+        else:
+            base = float(base)
+            height = float(height)
+            return 0.5 * base * height
--- EvanCarlile11_1/test_area.py
+++ EvanCarlile11_1/test_area.py
@@ -1,39 +1,54 @@
 import unittest
 import area as a
 
+
 class TestCase(unittest.TestCase):
     def test_square(self):
-        self.assertEqual(a.square(2),4)
-        self.assertEqual(a.square(33.33),1110.8889)
-        self.assertEqual(a.square(0),0)
+        self.assertEqual(a.square(2), 4)
+        self.assertAlmostEqual(a.square(33.33), 1110.8889, 5)
         self.assertRaises(ValueError, a.square, "foo")
         self.assertRaises(TypeError, a.square, -3)
+
     def test_circle(self):
-        self.assertAlmostEqual(a.circle(2),12.56637, 5)
+        self.assertAlmostEqual(a.circle(2), 12.56637, 5)
         self.assertAlmostEqual(a.circle(33.33), 3489.96041, 5)
-        self.assertEqual(a.circle(0), 0)
         self.assertAlmostEqual(a.circle(1), 3.14159, 5)
         self.assertRaises(ValueError, a.circle, "foo")
         self.assertRaises(TypeError, a.circle, -11)
+        self.assertRaises(TypeError, a.circle, 0)
+
     def test_rectangle(self):
-        self.assertEqual(a.rectangle(2,2),4)
-        self.assertEqual(a.rectangle(33.33,16.66),555.2778)
-        self.assertEqual(a.rectangle(0, 0),0)
-        self.assertEqual(a.rectangle(1, 0),0)
-        self.assertEqual(a.rectangle(0, 9999999),0)
+        self.assertEqual(a.rectangle(2, 2), 4)
+        self.assertAlmostEqual(a.rectangle(33, 16.66), 549.78, 2)
+        self.assertAlmostEqual(a.rectangle(16.66, 2), 33.32, 2)
+        self.assertAlmostEqual(a.rectangle(33.33, 16.66), 555.2778, 4)
         self.assertRaises(ValueError, a.rectangle, "foo", 0)
         self.assertRaises(ValueError, a.rectangle, 1, "foo")
         self.assertRaises(ValueError, a.rectangle, "bar", "foo")
         self.assertRaises(TypeError, a.rectangle, -2, -3)
+        self.assertRaises(TypeError, a.rectangle, 2, -3)
+        self.assertRaises(TypeError, a.rectangle, -2, 3)
+        self.assertRaises(TypeError, a.rectangle, 0, 1)
+        self.assertRaises(TypeError, a.rectangle, 1, 0)
 
     def test_triangle(self):
         self.assertEqual(a.triangle(2, 2), 2)
-        self.assertEqual(a.triangle(33.33, 16.66), 277.6389)
-        self.assertEqual(a.triangle(0, 0), 0)
-        self.assertEqual(a.triangle(1, 0),0)
-        self.assertEqual(a.triangle(0, 9999999), 0)
+        self.assertAlmostEqual(a.triangle(33.33, 16.66), 277.6389, 4)
+        self.assertAlmostEqual(a.triangle(33, 2.22), 36.63, 2)
+        self.assertAlmostEqual(a.triangle(5.55, 100), 277.5, 1)
         self.assertRaises(ValueError, a.triangle, "foo", 0)
         self.assertRaises(ValueError, a.triangle, 1, "foo")
         self.assertRaises(ValueError, a.triangle, "bar", "foo")
         self.assertRaises(TypeError, a.triangle, -2, -3)
         self.assertRaises(TypeError, a.triangle, 1, -3)
+        self.assertRaises(TypeError, a.triangle, -1, 1)
+        self.assertRaises(TypeError, a.triangle, 0, 1)
+        self.assertRaises(TypeError, a.triangle, 1, 0)
+
+
+def main():
+    unittest.main()
+
+
+if __name__ == "__main__":
+    main()
