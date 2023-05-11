7510c1710360dd06192b3e802874edbae405a0a7
Evan Carlile <114954921+ecar33@users.noreply.github.com>
2023-05-11 13:58:15 -0500

    Added docstrings for main.py

--- FinalProject_1/main.py
+++ FinalProject_1/main.py
@@ -2,6 +2,15 @@ from controller import *
 
 
 def main():
+    """
+    The entry point of the application.
+
+    This function initializes the QApplication, creates an instance of the Controller
+    class (which is the main window of the application), sets its fixed size and window
+    title, and finally displays the window. It then enters the application's event loop
+    by calling exec_() on the QApplication instance.
+    """
+
     application = QApplication([])
     window = Controller()
     window.setFixedSize(400, 550)
